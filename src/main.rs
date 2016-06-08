efbffb9cdd576aafb378431fb4e398420ee08c0c
Alexander Ronald Altman <alexanderaltman@me.com>
2016-06-07 17:06:40 -0700

    Argument parsing!

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::io;
+use std::io::prelude::*;
+use std::fs;
+
+#[macro_use]
 extern crate clap;
+use clap::{App, Arg};
+
+extern crate colored;
+use colored::*;
+
 extern crate difference_engine;
+// use difference_engine;
 
 fn main() {
-    println!("Hello, world!");
+  use clap::AppSettings::*;
+  let arg_matches = App::new("DEng")
+    .version(crate_version!())
+    .author("Alexander Altman")
+    .about("An extensible language-aware diff")
+    .settings(&[ColoredHelp])
+    .args(&[Arg::with_name("show languages")
+              .long("show-languages")
+              .short("L")
+              .help("Lists all of the languages currently supported by DEng")
+              .conflicts_with_all(&["language name", "old file", "new file"]),
+            Arg::with_name("language name")
+              .long("language")
+              .short("l")
+              .help("The language to use when examining the supplied files for differences")
+              .takes_value(true),
+            Arg::with_name("old file")
+              .help("The file to consider “old” for the purposes of finding \
+                     differences.{n}DEng will interpret a ‘-’ here as standard input.")
+              .required(true),
+            Arg::with_name("new file")
+              .help("The file to consider “new” for the purposes of finding \
+                     differences.{n}DEng will interpret a ‘-’ here as standard input.")
+              .required(true)])
+    .get_matches();
 }
